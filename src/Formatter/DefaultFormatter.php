ba82c6ebca68a49b20c83c78dc880e9995e9f1c8
Marc Blanc-Patin <marc.blancpatin@bpmconcept.com>
2016-03-27 21:57:12 +0200

    Allows to customize messages

---
+++ src/Formatter/DefaultFormatter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MarcBP\PingThis\Formatter;
+
+use MarcBP\PingThis\Ping\PingInterface;
+
+class DefaultFormatter implements FormatterInterface
+{
+    public function formatErrorMessage(\DateTime $date, PingInterface $ping, $newAlarm)
+    {
+        if ($newAlarm && $error = $ping->getLastError()) {
+            $subject = $error;
+        } else {
+            $subject = $newAlarm ? 'alarm triggered' : 'end of alarm';
+        }
+
+        return sprintf('[%s][%s] %s', $date->format('Y-m-d H:i:s'), $ping->getName(), $subject);
+    }
+}
---
+++ src/Formatter/FormatterInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MarcBP\PingThis\Formatter;
+
+use MarcBP\PingThis\Ping\PingInterface;
+
+interface FormatterInterface
+{
+    /**
+     * Format an error message that will be forwarded to the end user thanks
+     * to the alarm.
+     *
+     * @param $date         Date of the event
+     * @param $ping         Instance of the ping that raised the alarm
+     * @param $newAlarm     Boolean indicating if it is the begin or the end
+     *                      of an alarm
+     */
+    public function formatErrorMessage(\DateTime $date, PingInterface $ping, $newAlarm);
+}
