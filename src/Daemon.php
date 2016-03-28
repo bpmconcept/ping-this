d15021956c16c2807dded0b76692b4e67c5e3746
Marc Blanc-Patin <marc.blancpatin@bpmconcept.com>
2016-03-28 15:52:01 +0200

    Loop can be run outside

--- src/Daemon.php
+++ src/Daemon.php
@@ -38,30 +38,34 @@ class Daemon
         $this->formatter = $formatter;
         $this->alarm->setFormatter($this->formatter);
     }
+    
+    public function runOnce()
+    {
+        foreach ($this->pings as $ping) {
+            if ((time() - $this->lastCheck[$ping]) >= $ping->getPingFrequency()) {
+                $this->lastCheck[$ping] = time();
+
+                // This ping triggers an error
+                if (!$ping->ping()) {
+                    if (!$this->inErrorState->contains($ping)) {
+                        $this->inErrorState->attach($ping);
+                        $this->alarm->start($ping);
+                    }
+                }
+
+                // This ping instance was in error state
+                elseif ($this->inErrorState->contains($ping)) {
+                    $this->inErrorState->detach($ping);
+                    $this->alarm->stop($ping);
+                }
+            }
+        }
+    }
 
     public function run()
     {
         while (1) {
-            foreach ($this->pings as $ping) {
-                if ((time() - $this->lastCheck[$ping]) >= $ping->getPingFrequency()) {
-                    $this->lastCheck[$ping] = time();
-
-                    // This ping triggers an error
-                    if (!$ping->ping()) {
-                        if (!$this->inErrorState->contains($ping)) {
-                            $this->inErrorState->attach($ping);
-                            $this->alarm->start($ping);
-                        }
-                    }
-
-                    // This ping instance was in error state
-                    elseif ($this->inErrorState->contains($ping)) {
-                        $this->inErrorState->detach($ping);
-                        $this->alarm->stop($ping);
-                    }
-                }
-            }
-
+            $this->runOnce();
             sleep(1);
         }
     }
