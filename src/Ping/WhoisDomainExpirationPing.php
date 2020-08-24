99ef3a5c077cd10f7c39f1d9105782d0ce558fe1
Marc Blanc-Patin <marc.blancpatin@bpmconcept.com>
2020-08-24 10:33:04 +0200

    New whois request ping

---
+++ src/Ping/WhoisDomainExpirationPing.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace PingThis\Ping;
+
+class WhoisDomainExpirationPing extends StreamSocketCommandPing
+{
+    protected $domain;
+    protected $server;
+    protected $threshold;
+
+    public function __construct(int $frequency, string $domain, string $server, string $threshold)
+    {
+        $this->domain = $domain;
+        $this->server = $server;
+        $this->threshold = $threshold;
+
+        parent::__construct($frequency, "tcp://$server:43", "$domain\r\n", [$this, 'checkDate']);
+    }
+
+    public function getName(): string
+    {
+        return sprintf('Domain expiration date of %s', $this->domain);
+    }
+
+    protected function checkDate($response, &$error)
+    {
+        if (!preg_match('/.*Expiration Date.*: ([0-9TZ:\.\-]+)/', $response, $matches)) {
+            $error = "Expiration date not found";
+            return false;
+        }
+
+        if (new \DateTime($matches[1]) < new \DateTime($this->threshold)) {
+            $this->error = sprintf('Domain expires on %s', $this->date->format('Y-m-d H:i:s'));
+            return false;
+        }
+
+        return true;
+    }
+}
