781b45d9d19408fe14b8075627202348f838e9b8
Marc Blanc-Patin <marc.blancpatin@bpmconcept.com>
2021-03-29 12:04:38 +0200

    More verbose error message

--- src/Ping/HttpPing.php
+++ src/Ping/HttpPing.php
@@ -9,6 +9,12 @@ class HttpPing extends WebScraperPing
 {
     public function __construct(int $frequency, string $method, string $uri, int $code = 200)
     {
-        parent::__construct($frequency, $method, $uri, "response.getStatusCode() === $code");
+        parent::__construct($frequency, $method, $uri, function ($response, $content, &$error) use ($code) {
+            if ($response->getStatusCode() !== $code) {
+                $error = sprintf('Unvalid response code %d', $response->getStatusCode());
+                return false;
+            }
+            return true;
+        });
     }
 }
