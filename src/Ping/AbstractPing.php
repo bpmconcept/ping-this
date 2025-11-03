376fd76390d579be5b777fff742db619f3f81a24
Marc Blanc-Patin <marc.blancpatin@bpmconcept.com>
2025-11-03 10:25:00 +0100

    Refactor AbstractPing to use defaultAttempts

--- src/Ping/AbstractPing.php
+++ src/Ping/AbstractPing.php
@@ -8,10 +8,11 @@ abstract class AbstractPing implements PingInterface
 {
     protected int $attempts;
     protected ExpressionLanguage $language;
+    protected static int $defaultAttempts = 3;
 
     public function __construct(protected readonly int $frequency)
     {
-        $this->attempts = 3;
+        $this->attempts = self::$defaultAttempts;
         $this->language = new ExpressionLanguage();
     }
 
@@ -28,6 +29,20 @@ abstract class AbstractPing implements PingInterface
     public function getMaxAttemptsBeforeAlarm(): int
     {
         return $this->attempts;
+    }
+
+    public static function setDefaultMaxAttemptsBeforeAlarm(int $attempts): void
+    {
+        if ($attempts < 1) {
+            throw new \InvalidArgumentException('Default attempts must be greater than or equal to 1.');
+        }
+
+        self::$defaultAttempts = $attempts;
+    }
+
+    public static function getDefaultMaxAttemptsBeforeAlarm(): int
+    {
+        return self::$defaultAttempts;
     }
 
     protected function evaluate($expression, $data)
